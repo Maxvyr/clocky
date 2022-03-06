7214c27d07e0125dc2dd4ca1c985a75456eb3899
maxvyr <maxvid30@gmail.com>
2022-03-06 14:57:53 +0100

    :sarkles: first clock analogique

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+const hourShow = document.querySelector('.dig');
 
 const setDate = () => {
   const now = new Date();
 
-  const seconds = now.getSeconds();
+  let seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
+  
   const mins = now.getMinutes();
   const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-
+  
   const hour = now.getHours();
   const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  if(seconds < 10){
+    hourShow.innerHTML = `${hour} : ${mins} : 0${seconds}`;
+  } else {
+    hourShow.innerHTML = `${hour} : ${mins} : ${seconds}`;
+  }
 }
 
 setInterval(setDate, 1000);
